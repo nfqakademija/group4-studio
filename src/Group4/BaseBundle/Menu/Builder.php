47fe8ded0a5859e0d2fb54c979c24964d506e85a
ferven <andrius.bil@hotmail.lt>
2014-04-14 19:41:37 +0300

    Navbar a little bit fixed #2

--- src/Group4/BaseBundle/Menu/Builder.php
+++ src/Group4/BaseBundle/Menu/Builder.php
@@ -9,26 +9,20 @@ class Builder extends ContainerAware
     public function mainMenu(FactoryInterface $factory, array $options)
     {
         // Menu will be a navbar menu anchored to right
-        $menu = $factory->createItem('root', array(
-            'navbar' => true,
-            'pull-right' => true,
-        ));
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class','navbar nav');
+        $securityContext = $this->container->get('security.context');
+        $user = $securityContext->getToken()->getUser();
+        if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $menu->addChild($user->getUsername(),array('route' => 'fos_user_profile_show'));
+            $menu->addChild('Sign out', array('route' => 'fos_user_security_logout'));
+        } else {
+            $menu->addChild('Sign in', array('route' => 'fos_user_security_login'));
+            $menu->addChild('Sign up', array('route' => 'fos_user_registration_register'));
+        }
+
 
         // Add a regular child with an icon, icon- is prepended automatically
-        $layout = $menu->addChild('Layout', array(
-            'icon' => 'home',
-        ));
-
-        // Create a dropdown with a caret
-        $dropdown = $menu->addChild('Forms', array(
-            'dropdown' => true,
-            'caret' => true,
-        ));
-
-        // Create a dropdown header
-        $dropdown->addChild('Some Header', array('dropdown-header' => true));
-        $dropdown->addChild('Example 1');
-
         return $menu;
     }
 }

db66686037b6300627d2f1ac7d5d1528070e47c6
Justinas <Kazanaviius>
2014-04-24 17:05:17 +0300

    voteDate setting improvements

--- src/Group4/ChallengeBundle/Controller/PlayerController.php
+++ src/Group4/ChallengeBundle/Controller/PlayerController.php
@@ -249,13 +249,16 @@ class PlayerController extends Controller
                 $user = $this->container->get('security.context')->getToken()->getUser();
                 $em = $this->getDoctrine()->getManager();
                 $event = $em->getRepository('ChallengeBundle:PlayerToChallenge');
+                $challenge = $em->getRepository('ChallengeBundle:Challenge');
 
                 $pl2ch = $event->findOneBy(
                     array('user' => $user, 'challenge' => $eventId)
                 );
+                $ch = $challenge->findOneBy(array('id' => $eventId));
 
                 $pl2ch->setStatus('1');
                 $pl2ch->setImage($photo);
+                $ch->doVoteDateStuff();
                 $em->persist($pl2ch);
                 $em->flush();
 
